515db0c3354dc3fbba22c46d8e2b1a2f7cfca2a5
FrederickNH <123436129+FrederickNH@users.noreply.github.com>
2023-09-11 11:54:31 +0700

    Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            ProdukSeeder::class,
+            MerkSeeder::class
+        ]);
         // User::factory(10)->create();
     }
 }
