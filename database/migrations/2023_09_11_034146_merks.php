df50b3be83210d83ef996818fa686481066c78d4
chelseaclrst <chelseaclaristavalencia@gmail.com>
2023-09-11 10:44:20 +0700

    m

---
+++ database/migrations/2023_09_11_034146_merks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Merks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merks', function(Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        {
+            Schema::dropIfExists('merks');
+        }
+    }
+}
