5cb5041433eacc47906c885eb66260cee5fe6d7b
FrederickNH <123436129+FrederickNH@users.noreply.github.com>
2023-10-09 10:28:48 +0700

    template conquer

--- resources/views/laporanTransaksi/index.blade.php
+++ resources/views/laporanTransaksi/index.blade.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<table>
-    @foreach($queryBuilder as $laporanTransaksi)
-    <tr>
-        <th>{{$laporanTransaksi->id}}</th>
-        <th>{{$laporanTransaksi->nama}}</th>
-        <th>{{$laporanTransaksi->grand_total}}</th>
-    </tr>
-    @endforeach
-</table>
-</body>
-</html>
+@extends('layouts.conquer')
+
+@section('content')
+
+    <table>
+        @foreach($queryBuilder as $laporanTransaksi)
+        <tr>
+            <th>{{$laporanTransaksi->id}}</th>
+            <th>{{$laporanTransaksi->nama}}</th>
+            <th>{{$laporanTransaksi->grand_total}}</th>
+        </tr>
+        @endforeach
+    </table>
+
+
+    <p>test</p>
+@endsection
